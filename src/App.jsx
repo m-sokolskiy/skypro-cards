b72a325e2cfb78eb5d610da343471fcdc65016e3
m-sokolskiy <rognarius@icloud.com>
2025-07-05 20:00:04 +0300

    Установил шрифты

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { GlobalStyle } from "./GlobalStyle";
+
+const App = () => {
+
+  return (
+    <>
+      <h1 style={{ color: 'black' }} >Привет</h1>
+      <GlobalStyle />
+    </>
+  )
+
+}
+
+export default App;
